d92cc8810a4bc1fccc801f50cf4f84f9afceb755
Oleksandr Hulyi <agulyj@live.com>
2018-05-17 09:16:28 +0300

    Added code

---
+++ AsyncTeleport/AsyncRunner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.Linq;
+
+namespace System
+{
+    public class AsyncTeleport
+    {
+        private readonly List<Func<CancellationToken, Task>> _waitList = new List<Func<CancellationToken, Task>>();
+
+        private AsyncTeleport()
+        { }
+
+        internal void AddTerminator(Func<CancellationToken, Task> task)
+        {
+            _waitList.Add(task);
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        internal async Task Execute(Func<CancellationToken, Task> task)
+        {
+            await Execute(ct => VoidWrapper(task, ct));
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        private async Task<bool> VoidWrapper(Func<CancellationToken, Task> task, CancellationToken ct)
+        {
+            await task(ct);
+            return false;
+        }
+
+#if !DEBUG
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+#endif
+        internal async Task<T> Execute<T>(Func<CancellationToken, Task<T>> task)
+        {
+            var result = default(T);
+
+            var tcs = new CancellationTokenSource();
+
+            var mainTask = task(tcs.Token);
+
+            var tasksToWait = new List<Task> { mainTask };
+            tasksToWait.AddRange(_waitList.Select(t => t(tcs.Token)));
+
+            var terminator = WaitForTerminators(tasksToWait, tcs);
+
+            await Task.WhenAll(new[] { terminator, mainTask });
+
+            if (mainTask.Status == TaskStatus.RanToCompletion)
+                result = mainTask.GetAwaiter().GetResult();
+
+            return result;
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        private async Task WaitForTerminators(List<Task> terminators, CancellationTokenSource cts)
+        {
+            await Task.WhenAny(terminators);
+            cts.Cancel();
+        }
+
+        public static AsyncTeleport New()
+        {
+            return new AsyncTeleport();
+        }
+    }
+}
---
+++ AsyncTeleport/AsyncRunnerExtensions.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics;
+using System.Runtime.Loader;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class AsyncTeleportExtensions
+    {
+        private static readonly TaskCompletionSource<bool> _exitTcs;
+
+        static AsyncTeleportExtensions()
+        {
+            _exitTcs = new TaskCompletionSource<bool>();
+            AssemblyLoadContext.Default.Unloading += c => _exitTcs.SetResult(true);
+        }
+
+        public static AsyncTeleport CancelOn(this AsyncTeleport runner, Action method)
+        {
+            runner.AddTerminator(ct => Task.Run(method, ct));
+            return runner;
+        }
+
+        public static AsyncTeleport CancelOn(this AsyncTeleport runner, Func<CancellationToken, Task> task)
+        {
+            runner.AddTerminator(task);
+            return runner;
+        }
+
+        public static AsyncTeleport CancelOnSigTerm(this AsyncTeleport runner, Action react = null)
+        {
+            var term = _exitTcs.Task;
+
+            if (react != null)
+                term = term.ContinueWith(t => { react(); return t.Result; });
+
+            runner.AddTerminator(ct => _exitTcs.Task);
+            return runner;
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        public static async Task Wait(this CancellationToken ct)
+        {
+            while (!ct.IsCancellationRequested)
+                try
+                {
+                    await Task.Delay(Int32.MaxValue, ct).ContinueWith(t => { }, TaskContinuationOptions.OnlyOnCanceled);
+                }
+                catch (TaskCanceledException) { }
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        public static T Run<T>(this AsyncTeleport runner, Func<CancellationToken, Task<T>> task)
+        {
+            return ExecuteSync(runner, task);
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        public static T Run<T, TA1>(this AsyncTeleport runner, Func<TA1, CancellationToken, Task<T>> task, TA1 a1)
+        {
+            return ExecuteSync(runner, ct => task(a1, ct));
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        public static T Run<T, TA1, TA2>(this AsyncTeleport runner, Func<TA1, TA2, CancellationToken, Task<T>> task, TA1 a1, TA2 a2)
+        {
+            return ExecuteSync(runner, ct => task(a1, a2, ct));
+        }
+
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        public static void Run(this AsyncTeleport runner, Func<CancellationToken, Task> task)
+        {
+            ExecuteSync(runner, task);
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        public static void Run<TA1>(this AsyncTeleport runner, Func<TA1, CancellationToken, Task> task, TA1 a1)
+        {
+            ExecuteSync(runner, ct => task(a1, ct));
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        public static void Run<TA1, TA2>(this AsyncTeleport runner, Func<TA1, TA2, CancellationToken, Task> task, TA1 a1, TA2 a2)
+        {
+            ExecuteSync(runner, ct => task(a1, a2, ct));
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        private static T ExecuteSync<T>(AsyncTeleport runner, Func<CancellationToken, Task<T>> task)
+        {
+            return runner.Execute(task).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        private static void ExecuteSync(AsyncTeleport runner, Func<CancellationToken, Task> task)
+        {
+            runner.Execute(task).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+    }
+}
